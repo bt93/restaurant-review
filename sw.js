7ce05603a261144aba6cbed11aedb5c41eaf671c
Jason Howie <howiejason93@gmail.com>
2018-11-05 16:00:11 -0500

    add /restaurant-review/ to path

--- sw.js
+++ sw.js
@@ -3,23 +3,23 @@ self.addEventListener('install', (event) => {
 	event.waitUntil(
 		caches.open('v1').then( (cache) => {
 			return cache.addAll([
-				'index.html',
-				'restaurant.html',
-				'/data/restaurants.json',
-				'/js/dbhelper.js',
-				'/js/main.js',
-				'/js/restaurant_info.js',
-				'/js/swinit.js',
-				'/css/styles.css',
-				'/img/1.jpg',
-				'/img/2.jpg',
-				'/img/3.jpg',
-				'/img/4.jpg',
-				'/img/5.jpg',
-				'/img/6.jpg',
-				'/img/7.jpg',
-				'/img/8.jpg',
-				'/img/9.jpg'
+				'/restaurant-review/index.html',
+				'/restaurant-review/restaurant.html',
+				'/restaurant-review//data/restaurants.json',
+				'/restaurant-review//js/dbhelper.js',
+				'/restaurant-review//js/main.js',
+				'/restaurant-review//js/restaurant_info.js',
+				'/restaurant-review//js/swinit.js',
+				'/restaurant-review//css/styles.css',
+				'/restaurant-review//img/1.jpg',
+				'/restaurant-review//img/2.jpg',
+				'/restaurant-review//img/3.jpg',
+				'/restaurant-review//img/4.jpg',
+				'/restaurant-review//img/5.jpg',
+				'/restaurant-review//img/6.jpg',
+				'/restaurant-review//img/7.jpg',
+				'/restaurant-review//img/8.jpg',
+				'/restaurant-review//img/9.jpg'
 			]);
 		})
 	);
